43d1542d1870db92efd3028b235eee1a6e9b8b78
YukiMatheus <matheus.yuki@hotmail.com>
2023-02-12 12:52:44 -0300

    musica

---
+++ src/main/java/com/tech4music/musica/MusicaDto.java
@@ -0,0 +1,13 @@
+package com.tech4music.musica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicaDto {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicaDto.class, args);
+	}
+
+}
